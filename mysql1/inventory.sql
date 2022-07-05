95b40c68ec467f40c32474a08f2aa310cab4ab2a
orip <orip@wix.com>
2022-07-05 13:27:00 +0300

    fix auto gtid replication

--- mysql1/inventory.sql
+++ mysql1/inventory.sql
@@ -4,14 +4,16 @@
 # However, this grant is equivalent to specifying *any* hosts, which makes this easier since the docker host
 # is not easily known to the Docker container. But don't do this in production.
 #
-CREATE USER 'replicator' IDENTIFIED BY 'replpass';
-CREATE USER 'debezium' IDENTIFIED BY 'dbz';
-GRANT REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO 'replicator';
-GRANT SELECT, RELOAD, SHOW DATABASES, REPLICATION SLAVE, REPLICATION CLIENT  ON *.* TO 'debezium';
+# CREATE USER 'replicator' IDENTIFIED BY 'replpass';
+# CREATE USER 'debezium' IDENTIFIED BY 'dbz';
+# GRANT REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO 'replicator';
+# GRANT SELECT, RELOAD, SHOW DATABASES, REPLICATION SLAVE, REPLICATION CLIENT  ON *.* TO 'debezium';
 
 # Create the database that we'll use to populate data and watch the effect in the binlog
-CREATE DATABASE inventory;
-GRANT ALL PRIVILEGES ON inventory.* TO 'mysqluser'@'%';
+# CREATE DATABASE inventory;
+# GRANT ALL PRIVILEGES ON inventory.* TO 'mysqluser'@'%';
 
 # Switch to this database
-USE inventory;
+# USE inventory;
+
+DROP USER mysqluser;
